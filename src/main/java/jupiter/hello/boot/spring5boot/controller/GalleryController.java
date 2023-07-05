db5b380be044b6f7e22a5814d9cf4d600e57058b
jupiter002 <jik7194@naver.com>
2023-07-05 12:18:11 +0900

    페이지 맵핑 부분 추가

---
+++ src/main/java/jupiter/hello/boot/spring5boot/controller/GalleryController.java
@@ -0,0 +1,18 @@
+package jupiter.hello.boot.spring5boot.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/gallery")
+@Controller
+public class GalleryController {
+    Logger logger = LogManager.getLogger(GalleryController.class);
+    @GetMapping("/list")
+    public String list(){
+        logger.info("gallery/list 호출");
+        return "gallery/list";
+    }
+}
---
+++ src/main/java/jupiter/hello/boot/spring5boot/controller/JoinController.java
@@ -0,0 +1,18 @@
+package jupiter.hello.boot.spring5boot.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/join")
+@Controller
+public class JoinController {
+    Logger logger = LogManager.getLogger(JoinController.class);
+    @GetMapping("/agree")
+    public String join(){
+        logger.info("join/agree 호출");
+        return "join/agree";
+    }
+}
