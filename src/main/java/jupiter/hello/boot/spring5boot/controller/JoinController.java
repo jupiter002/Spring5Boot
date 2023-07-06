01bf1e253aac8092966aa6d00c499286d219880b
jupiter002 <jik7194@naver.com>
2023-07-06 15:51:23 +0900

    회원가입: 실명확인 처리

--- src/main/java/jupiter/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/jupiter/hello/boot/spring5boot/controller/JoinController.java
@@ -1,10 +1,14 @@
 package jupiter.hello.boot.spring5boot.controller;
 
+import jupiter.hello.boot.spring5boot.model.Checkme;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequestMapping("/join")
 @Controller
@@ -19,6 +23,19 @@ public class JoinController {
     public String checkme(){
         logger.info("join/checkme 호출");
         return "join/checkme";
+    }
+    @PostMapping("/checkme")
+    public String checkmeok(Checkme checkme, HttpSession sess){
+        logger.info("join/checkmeok 호출");
+        // checkme에 보낸 개인정보를 세션에 저장하고
+        // joinme로 이동
+        String viewPage = "redirect:/join/checkme";
+        if(checkme.getName()!=null){
+            sess.setAttribute("checkme",checkme);
+            viewPage = "redirect:/join/joinme";
+        }
+
+        return viewPage;
     }
     @GetMapping("/joinme")
     public String joinme(){
