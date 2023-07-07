8c94988cb52c33b4a2f47437328795b234658842
jupiter002 <jik7194@naver.com>
2023-07-07 10:39:04 +0900

    회원가입: Member 클래스 추가

---
+++ src/main/java/jupiter/hello/boot/spring5boot/model/Member.java
@@ -0,0 +1,22 @@
+package jupiter.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Member {
+    public String mno;
+    public String userid;
+    public String passwd;
+    public String name;
+    public String email;
+    public String zipcode;
+    public String addr1;
+    public String addr2;
+    public String phone;
+    public String regdate;
+
+}
