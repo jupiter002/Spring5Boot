ea70d41046b14de5b0b1ce42c113236c838f2cb7
jupiter002 <jik7194@naver.com>
2023-07-07 10:39:46 +0900

    회원가입: MemberDAO 추가

---
+++ src/main/java/jupiter/hello/boot/spring5boot/dao/MemberDAO.java
@@ -0,0 +1,10 @@
+package jupiter.hello.boot.spring5boot.dao;
+
+import jupiter.hello.boot.spring5boot.model.Member;
+
+import java.util.List;
+
+public interface MemberDAO {
+    int insertMember(Member m);
+    List<Member> selectMember();
+}
