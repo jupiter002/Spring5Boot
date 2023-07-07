300ff54a9618ec25f0bb41000315f45339f05e8b
jupiter002 <jik7194@naver.com>
2023-07-07 11:57:40 +0900

    회원가입: MemberMapper 인터페이스 추가

---
+++ src/main/java/jupiter/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -0,0 +1,11 @@
+package jupiter.hello.boot.spring5boot.mybatis;
+
+import jupiter.hello.boot.spring5boot.model.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface MemberMapper {
+    int insertMember(Member m);
+    List<Member> selectMember();
+}
