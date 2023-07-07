df75a51b5b534c7ad4af7b151e556d67fc856d35
jupiter002 <jik7194@naver.com>
2023-07-07 11:59:55 +0900

    회원가입: MemberMapper 맵핑 namespcace 변경 MemberDAOImpl Mapper 호출방식 변경

--- src/main/java/jupiter/hello/boot/spring5boot/dao/MemberDAOImpl.java
+++ src/main/java/jupiter/hello/boot/spring5boot/dao/MemberDAOImpl.java
@@ -1,14 +1,22 @@
 package jupiter.hello.boot.spring5boot.dao;
 
 import jupiter.hello.boot.spring5boot.model.Member;
+
+import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import jupiter.hello.boot.spring5boot.mybatis.MemberMapper;
 
 import java.util.List;
 @Repository("mdao")
+@RequiredArgsConstructor
 public class MemberDAOImpl implements MemberDAO{
     // mybatis를 사용하기 위해 필요한 객체 DI
+    // 단, 생성자 주입 방식 사용
+    @Autowired
+    final MemberMapper memberMapper;
+
     @Autowired
     private SqlSession sqlSession;
 
@@ -16,7 +24,9 @@ public class MemberDAOImpl implements MemberDAO{
     public int insertMember(Member m) {
 
         // insert(insert관련 맵핑, 매개변수)
-        return sqlSession.insert("member.inserMember",m);
+        // sqlSession.insert("insertMember",m) 로 사용하는 방식보다는 편리함
+        //return sqlSession.insert("MemberMapper.insertMember",m);
+        return memberMapper.insertMember(m);
     }
 
     @Override
