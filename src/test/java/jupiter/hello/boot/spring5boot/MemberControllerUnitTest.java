916e356804fd8cb1f682c7bdf5409ad1d4ef9936
jupiter002 <jik7194@naver.com>
2023-07-11 11:59:24 +0900

    로그인: JoinController login 단위테스트

--- src/test/java/jupiter/hello/boot/spring5boot/MemberControllerUnitTest.java
+++ src/test/java/jupiter/hello/boot/spring5boot/MemberControllerUnitTest.java
@@ -37,6 +37,16 @@ public class MemberControllerUnitTest {
                 .param("phone",""))
                 .andExpect(status().is3xxRedirection())
                 .andDo(print());
+    }
+    @Test
+    @DisplayName("MemberController login Test")
+    void login() throws Exception {
+
+        mvc.perform(post("/join/login")
+                        .param("userid","abc123")
+                        .param("passwd","987xyz"))
+                .andExpect(status().is3xxRedirection())
+                .andDo(print());
 
     }
 
