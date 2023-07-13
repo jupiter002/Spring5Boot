92662a77d61931b0ed2a8358d5b6db538039d402
jupiter002 <jik7194@naver.com>
2023-07-13 16:06:17 +0900

    게시판: 검색 기능을 위한 코드 추가

--- src/main/java/jupiter/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/jupiter/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -4,6 +4,7 @@ import jupiter.hello.boot.spring5boot.model.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface BoardMapper {
@@ -12,5 +13,7 @@ public interface BoardMapper {
     Board selectOneBoard(String bno);
     int updateViewBoard(String bno);
     int selectCountBoard();
+    List<Board> selectFindBoard(Map<String, Object> params);
+
 
 }
