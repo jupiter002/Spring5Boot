a4163f418981dcf49563b46f2674314d17fa95d6
jupiter002 <jik7194@naver.com>
2023-07-14 16:05:03 +0900

    자료실 - Pds dto 생성

---
+++ src/main/java/jupiter/hello/boot/spring5boot/model/Pds.java
@@ -0,0 +1,19 @@
+package jupiter.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Pds {
+    private String pno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+}
