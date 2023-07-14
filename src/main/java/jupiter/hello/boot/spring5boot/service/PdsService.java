807d64f112d99a827ad0dcc07e50e0f5f9a6a5fc
jupiter002 <jik7194@naver.com>
2023-07-14 18:05:01 +0900

    자료실 - PdsServiceImpl : processUpload, insertPdsAttach 구현

--- src/main/java/jupiter/hello/boot/spring5boot/service/PdsService.java
+++ src/main/java/jupiter/hello/boot/spring5boot/service/PdsService.java
@@ -3,11 +3,10 @@ package jupiter.hello.boot.spring5boot.service;
 import jupiter.hello.boot.spring5boot.model.Pds;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
 
 public interface PdsService {
     int newPds(Pds p);
-    boolean newPdsAttatch(MultipartFile attatch, int pno);
+    boolean newPdsAttach(MultipartFile attach, int pno);
 
 
 
--- src/main/java/jupiter/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/jupiter/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -2,25 +2,31 @@ package jupiter.hello.boot.spring5boot.service;
 
 import jupiter.hello.boot.spring5boot.dao.PdsDAO;
 import jupiter.hello.boot.spring5boot.model.Pds;
+import jupiter.hello.boot.spring5boot.model.PdsAttach;
+import jupiter.hello.boot.spring5boot.utils.PdsUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Map;
 
 @Service("psrv")
 @RequiredArgsConstructor
 public class PdsServiceImpl implements PdsService {
 
     final PdsDAO pdao;
+    final PdsUtils pdsUtils;
     @Override
     public int newPds(Pds p) {
         return pdao.insertPds(p);
     }
 
     @Override
-    public boolean newPdsAttatch(MultipartFile attatch, int pno) {
-        return false;
+    public boolean newPdsAttach(MultipartFile attach, int pno) {
+        //첨부한 파일을 지정한 위치에 저장후 파일정보 리턴
+        PdsAttach pa = pdsUtils.processUpload(attach);
+        pa.setPno(pno + "");
+        //첨부파일 정보를 DB에 저장
+        int pacnt = pdao.insertPdsAttach(pa);
+        return (pacnt > 0 )? true : false;
     }
 
 
