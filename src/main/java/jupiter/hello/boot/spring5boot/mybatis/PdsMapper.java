b157eef980cac3fc7603e68706b9cdc628e6e614
jupiter002 <jik7194@naver.com>
2023-07-17 13:38:41 +0900

    자료실 - list, view : selectPds, selectOnePds 구현, resultMap으로 Pds, PdsAttach join결과 합치기

--- src/main/java/jupiter/hello/boot/spring5boot/mybatis/PdsMapper.java
+++ src/main/java/jupiter/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -4,11 +4,19 @@ import jupiter.hello.boot.spring5boot.model.Pds;
 import jupiter.hello.boot.spring5boot.model.PdsAttach;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface PdsMapper {
     int insertPds(Pds p);
     int lastPdsPno();
     int insertPdsAttach(PdsAttach pa);
+    List<Pds> selectPds(int stnum);
+    Pds selectOnePds(String pno);
+    int selectCountPds();
+
+
+
 
 
 
