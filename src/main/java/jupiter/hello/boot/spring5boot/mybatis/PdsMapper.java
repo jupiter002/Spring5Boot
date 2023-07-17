46752f3008f4b6c60a8e1f0ab97bf5bcc790a963
jupiter002 <jik7194@naver.com>
2023-07-17 10:54:32 +0900

    PdsMapper insertPdsAttach 매개변수 넣기

--- src/main/java/jupiter/hello/boot/spring5boot/mybatis/PdsMapper.java
+++ src/main/java/jupiter/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -1,21 +1,15 @@
 package jupiter.hello.boot.spring5boot.mybatis;
 
 import jupiter.hello.boot.spring5boot.model.Pds;
+import jupiter.hello.boot.spring5boot.model.PdsAttach;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PdsMapper {
-    int insertPds(Pds b);
+    int insertPds(Pds p);
     int lastPdsPno();
-    int insertPdsAttach();
+    int insertPdsAttach(PdsAttach pa);
 
-    /*List<Pds> selectPds(int stnum);
-    Pds selectOnePds(String bno);
-    int updateViewPds(String bno);
-    int selectCountPds();
-    List<Pds> selectFindPds(Map<String, Object> params);
-    int countFindPds(Map<String, Object> params);
-*/
 
 
 }
