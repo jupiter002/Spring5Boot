d4c18d661212a15b60eb6c5af11f32568b911f1b
jupiter002 <jik7194@naver.com>
2023-07-18 11:05:36 +0900

    자료실 다운로드 - PdsUtils : getHeader, getResource 구현

--- src/main/java/jupiter/hello/boot/spring5boot/utils/PdsUtils.java
+++ src/main/java/jupiter/hello/boot/spring5boot/utils/PdsUtils.java
@@ -4,11 +4,17 @@ import jupiter.hello.boot.spring5boot.model.PdsAttach;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -18,6 +24,33 @@ public class PdsUtils {
 
     // 첨부파일 저장위치
     @Value("${saveDir}") private String saveDir;
+
+    public HttpHeaders getHeader(String fname) {
+        HttpHeaders header = new HttpHeaders();
+        // 다운로드할 파일의 이름을 인코딩함 - 한글처리
+        fname = UriUtils.encode(fname, StandardCharsets.UTF_8);
+
+        try {
+            // 다운로드할 파일의 유형을 헤더에 저장
+            header.add("Content-Type", Files.probeContentType(Paths.get(saveDir + fname)));
+            header.add("Content-Dispositon","attachment; filename=" + fname);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return header;
+    }
+
+    public UrlResource getResource(String fname) {
+        UrlResource resource = null;
+        fname = UriUtils.encode(fname,StandardCharsets.UTF_8);
+        try{
+            resource = new UrlResource("file:" + saveDir + fname);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return resource;
+    }
 
 
     public PdsAttach processUpload(MultipartFile attach) {
