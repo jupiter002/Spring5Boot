08385d37c19bb027721f9c07648f31345054f9c4
jupiter002 <jik7194@naver.com>
2023-07-18 13:34:36 +0900

    댓글 - insertPdsComment, selectPdsComment 구현

--- src/main/java/jupiter/hello/boot/spring5boot/mybatis/PdsMapper.java
+++ src/main/java/jupiter/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -2,6 +2,7 @@ package jupiter.hello.boot.spring5boot.mybatis;
 
 import jupiter.hello.boot.spring5boot.model.Pds;
 import jupiter.hello.boot.spring5boot.model.PdsAttach;
+import jupiter.hello.boot.spring5boot.model.PdsComment;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -15,6 +16,9 @@ public interface PdsMapper {
     Pds selectOnePds(String pno);
     int selectCountPds();
     PdsAttach selectOnePdsAttach(String pno);
+    int insertPdsComment(PdsComment pc);
+
+    List<PdsComment> selectPdsComment(String pno);
 
 
 
